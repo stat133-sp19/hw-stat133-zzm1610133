1a9dc5385d8e9c477a86e374f70d0a36f7a869b6
zzm1610133 <zzm_1610133@berkeley.edu>
2019-04-17 21:33:55 -0600

    some little changes

--- workout02/ui.R
+++ workout02/ui.R
@@ -36,4 +36,3 @@ ui <- fluidPage(
   )
   
 )
-
--- workout02/workout02-Zimeng-Zheng.R
+++ workout02/workout02-Zimeng-Zheng.R
@@ -45,22 +45,22 @@ server <- function(input, output) {
     for(i in 1:input$year) fixed_contrib[i+1]<-fixed_contrib[i]*(1+input$return_rate/100)+input$contribution
     growing_contrib<-c(input$amount)
     for(i in 1:input$year) growing_contrib[i+1]<-growing_contrib[i]*(1+input$return_rate/100)+input$contribution*(1+input$growing_rate/100)^(i-1)
-    table<-data.frame(year=rep(0:input$year,3),value=c(no_contrib,fixed_contrib,growing_contrib),model=as.factor(rep(c('no_contrib','fixed_contrib','growing_contrib'),each=input$year+1)),ncol=3)
+    table<-data.frame(year=rep(0:input$year,3),value=c(no_contrib,fixed_contrib,growing_contrib),variable=as.factor(rep(c('no_contrib','fixed_contrib','growing_contrib'),each=input$year+1)),ncol=3)
     if(input$facet=='No'){  
       ggplot(table)+
-        geom_point(aes(x=year,y=value,col=model))+
-        geom_line(aes(x=year,y=value,col=model))+
-        labs(title = 'Three models of investing')+
+        geom_point(aes(x=year,y=value,col=variable))+
+        geom_line(aes(x=year,y=value,col=variable))+
+        labs(title = 'Three modes of investing')+
         scale_x_continuous(breaks=seq(0,input$year,2))
     }
     else{ 
       ggplot(table)+
-        geom_point(aes(x=year,y=value,col=model))+
-        geom_line(aes(x=year,y=value,col=model))+
-        geom_area(aes(x=year,y=value,fill=model),alpha=0.5)+
+        geom_point(aes(x=year,y=value,col=variable))+
+        geom_line(aes(x=year,y=value,col=variable))+
+        geom_area(aes(x=year,y=value,fill=variable),alpha=0.5)+
         scale_x_continuous(breaks=seq(0,input$year,2))+
-        labs(title = 'Three models of investing')+
-        facet_grid(~model)
+        labs(title = 'Three modes of investing')+
+        facet_grid(~variable)
     }
   })
   
